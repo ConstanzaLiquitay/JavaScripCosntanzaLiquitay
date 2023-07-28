07d7cd4b45325652d9148a7ea8fc83a191ef507c
Constanza <cotyliquitay@gmail.com>
2023-07-28 09:12:30 -0300

    java scrip

--- JS/js-proyecto.js
+++ JS/js-proyecto.js
@@ -1,61 +1,98 @@
+
+//let edad = prompt("Ingresa tu edad");
+
+//if (edad >= 18){
+//  alert ("Podes entrar a la pagina")
+//}
+//else{   document.body.style.display = "none";
+//alert("No puedes ingresar a la pagina porque sos menor")}
+
+
+
+
 let carrito = [];
-class productos{
-  constructor(imagen,bodega, titulo,descripcion,precio){
+
+class Productos {
+  constructor(imagen, bodega, titulo, descripcion, precio,cantidad) {
     this.imagen = imagen;
-    this.bodega = bodega
+    this.bodega = bodega;
     this.titulo = titulo;
     this.descripcion = descripcion;
     this.precio = precio;
+    this.cantidad = cantidad
   }
 }
 
-const descProductos = [
-new productos('./Img/Medall-trapiche.jpg', 'Bodega Trapiche','Vino Medalla', 'En 1983 en homenaje a los 100 años de la bodega, emerge este vino de excepción elaborado con uvas provenientes de la Primera Zona vitivinícola mendocina', 1200),
-new productos('./Img/vino.bodegagraffigna.jpg', 'Bodega Graffigna','Vino Malbec', 'Con un profundo color rojo y tintes violetas, este Malbec destaca por su intensidad frutal aromática de bayas maduras, especias dulces y pimienta, junto a notas tostadas de crianza en roble.', 5000),
-new productos('./IMG/vino4trapiche.png', 'Bodega Trapiche','Vino Pinot Noir', 'Esta línea representa la más delicada selección de uvas con el fin de obtener ediciones auténticas de la costa y la pampa.', 1400),
-]
+const getProduct = async () => {
+  const response = await fetch("./JS/productos.json");
+  const data = await response.json();
+  const descProductos = data.descProductos; // Accedemos al array de productos
 
-const productContainer = document.querySelector('#product-container');
+  const productContainer = document.querySelector('#product-container');
 
-descProductos.forEach(productos => {
-  const productDiv = document.createElement('div');
-  //innerHTML
-  productDiv.innerHTML = `
-  <div class ='card'>
-  <img src=${productos.imagen}></img>
-  <p>${productos.bodega}</p>
-  <h3>${productos.titulo}</h3>
-  <p>${productos.descripcion}</p>
-  <p>${productos.precio}</p>
-  <button class=comprar>Comprar</button>
-  <button class= vermas>Ver Mas</button>
-  </div>
-  `;
+  descProductos.forEach((producto) => {
+    const productDiv = document.createElement('div');
+    productDiv.innerHTML = `
+      <div class="containerProd">
+      <div class="container-img">
+        <img class="imgvino" src=${producto.imagen}></img>
+        </div>  
+        <p class="title-bodega">${producto.bodega}</p>
+        <h3 class="title-vino" >${producto.titulo}</h3>
+        <p class="description" >${producto.descripcion}</p>
+        <p class="precio">${producto.precio}</p>
+        <button class="comprar">Comprar</button>
+        <button class="vermas">Ver Mas</button>
+      </div>`;
 
+    productContainer.appendChild(productDiv);
 
-  productContainer.appendChild(productDiv);
+    const addButton = productDiv.querySelector('.comprar');
+    addButton.addEventListener('click', () => {
+      carrito.push(producto);
+      guardarcarrito(carrito);
+      mostrarcarrito();
+    });
+  });
+};
 
- const addButton =  productDiv.querySelector('.comprar');
-addButton.addEventListener('click',() =>{
-
-  carrito.push(productos);
-
-  //Guardamos al carrito
-  guardarcarrito(carrito)
-
-  const modal = document.getElementById('modal');
-  modal.innerHTML = `
-  <div class="modal-content">
-  <h3>${productos.titulo}</h3>
-  <p>${productos.descripcion}</p>
-  <p>${productos.precio}</p>
-  </div> `
-
-  modal.style.display = 'block'
-})
-
-})
-
-function guardarcarrito(carrito){
+function guardarcarrito(carrito) {
   localStorage.setItem('carrito', JSON.stringify(carrito));
 }
+
+function eliminarDelCarrito(index) {
+  carrito.splice(index, 1);
+  guardarcarrito(carrito);
+  mostrarcarrito();
+}
+
+function mostrarcarrito() {
+  const cartContainer = document.getElementById('cart-container');
+  const carritoGuardado = localStorage.getItem('carrito');
+  //const totalPedidoElement = document.getElementById('total-pedido');
+
+  // Limpiar el contenedor del carrito antes de mostrar los productos
+  cartContainer.innerHTML = '';
+
+  if (carritoGuardado) {
+    const carritoParseado = JSON.parse(carritoGuardado);
+    carritoParseado.forEach((producto, index) => {
+      const productDiv = document.createElement('div');
+      productDiv.innerHTML = `
+        <h3>${producto.titulo}</h3>
+        <p>${producto.descripcion}</p>
+        <p>${producto.precio}</p>
+        <span class=restar> - </span>
+        <p>${producto.cantidad}</p>
+        <span class=sumar> + </span>
+        <button class="eliminarCarrito" onclick="eliminarDelCarrito(${index})">Eliminar del Carrito</button>`;
+
+      cartContainer.appendChild(productDiv);
+    });
+  }
+  }
+
+
+
+getProduct();
+
