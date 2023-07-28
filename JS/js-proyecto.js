e91cba786430e1a9c7bea56b8356055ba1cee736
Constanza <cotyliquitay@gmail.com>
2023-07-28 17:57:06 -0300

    js

--- JS/js-proyecto.js
+++ JS/js-proyecto.js
@@ -1,19 +1,16 @@
 
 //let edad = prompt("Ingresa tu edad");
-
 //if (edad >= 18){
-//  alert ("Podes entrar a la pagina")
+//alert ("Podes entrar a la pagina")
 //}
 //else{   document.body.style.display = "none";
 //alert("No puedes ingresar a la pagina porque sos menor")}
 
 
-
-
 let carrito = [];
 
 class Productos {
-  constructor(imagen, bodega, titulo, descripcion, precio,cantidad) {
+  constructor(imagen, bodega, titulo, descripcion, precio, cantidad) {
     this.imagen = imagen;
     this.bodega = bodega;
     this.titulo = titulo;
@@ -26,8 +23,7 @@ class Productos {
 const getProduct = async () => {
   const response = await fetch("./JS/productos.json");
   const data = await response.json();
-  const descProductos = data.descProductos; // Accedemos al array de productos
-
+  const descProductos = data.descProductos;
   const productContainer = document.querySelector('#product-container');
 
   descProductos.forEach((producto) => {
@@ -42,16 +38,44 @@ const getProduct = async () => {
         <p class="description" >${producto.descripcion}</p>
         <p class="precio">${producto.precio}</p>
         <button class="comprar">Comprar</button>
-        <button class="vermas">Ver Mas</button>
       </div>`;
 
     productContainer.appendChild(productDiv);
 
+
+
     const addButton = productDiv.querySelector('.comprar');
     addButton.addEventListener('click', () => {
-      carrito.push(producto);
+
+      const repet = carrito.some((repetProduct) => repetProduct.titulo === producto.titulo);
+
+      if (repet) {
+        carrito.map((producto) => {
+          if (producto.titulo === producto.titulo) {
+            producto.cantidad++;
+          }
+        })
+      } else {
+
+        carrito.push({
+          imagen: producto.imagen,
+          bodega: producto.bodega,
+          titulo: producto.titulo,
+          descripcion: producto.descripcion,
+          precio: producto.precio,
+          cantidad: producto.cantidad,
+        })
+      }
+      Swal.fire(
+        'Producto Agregado al carrito!',
+        'Nos encanta el vinito que elegiste',
+        'success',
+      )
+
       guardarcarrito(carrito);
       mostrarcarrito();
+
+
     });
   });
 };
@@ -69,7 +93,6 @@ function eliminarDelCarrito(index) {
 function mostrarcarrito() {
   const cartContainer = document.getElementById('cart-container');
   const carritoGuardado = localStorage.getItem('carrito');
-  //const totalPedidoElement = document.getElementById('total-pedido');
 
   // Limpiar el contenedor del carrito antes de mostrar los productos
   cartContainer.innerHTML = '';
@@ -82,17 +105,31 @@ function mostrarcarrito() {
         <h3>${producto.titulo}</h3>
         <p>${producto.descripcion}</p>
         <p>${producto.precio}</p>
-        <span class=restar> - </span>
-        <p>${producto.cantidad}</p>
-        <span class=sumar> + </span>
+        <p>Cantidad: ${producto.cantidad}</p>
+        <p>Total: ${producto.cantidad * producto.precio}</p>
         <button class="eliminarCarrito" onclick="eliminarDelCarrito(${index})">Eliminar del Carrito</button>`;
 
       cartContainer.appendChild(productDiv);
-    });
-  }
-  }
+    })
+
+    //let restar = productDiv. querySelector(".restar");
+    //restar.addEventListener("click", () => {
+    //if (producto.cantidad > 0) { 
+    //producto.cantidad--;
+    //mostrarcarrito();
+    //}
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const totalcarrito = document.createElement("div");
+    totalcarrito.className = "total-content";
+    totalcarrito.innerHTML = `Total a pagar: ${total}$`;
+
+    cartContainer.appendChild(totalcarrito);
 
 
+  }//);
+};
+//}
 
 getProduct();
 
